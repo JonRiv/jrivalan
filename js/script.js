1bdc8bf41db5087d38498e14666f64559f196e07
JonRiv <jonathan.rivalan@gmail.com>
2016-12-04 19:19:37 +0100

    removed requestAnimationFrame polyfill

--- js/script.js
+++ js/script.js
@@ -409,30 +409,6 @@
       z = nbis+1;
     return Math.floor(Math.random()*(z-n)+n);
     }
-    //Initialise l'API d'animation HTML5 /*Credits : Erik Möller & Paul Irish*/
-    (function() {
-        var lastTime = 0;
-        var vendors = ['ms', 'moz', 'webkit', 'o'];
-        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-            window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-        }
-     
-        if (!window.requestAnimationFrame)
-            window.requestAnimationFrame = function(callback, element) {
-                var currTime = new Date().getTime();
-                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-                  timeToCall);
-                lastTime = currTime + timeToCall;
-                return id;
-            };
-     
-        if (!window.cancelAnimationFrame)
-            window.cancelAnimationFrame = function(id) {
-                clearTimeout(id);
-            };
-    }());
 
       //todo readd the relaunch button
       (function BubbleDemo(){
